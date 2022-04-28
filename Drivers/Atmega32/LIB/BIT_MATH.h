ad9874a434b8ed966c80b093d12545a23231891a
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-04-28 11:20:57 +0200

    Create BIT_MATH.h

---
+++ Drivers/Atmega32/LIB/BIT_MATH.h
@@ -0,0 +1,17 @@
+/************************************************************/
+/* Author : Nadine Ashraf Adeeb                             */
+/* Description: File that holds all configuration for       */
+/*                Header Guard File                         */
+/*                Layer : LIB                               */
+/* Version :v1.0                                            */
+/* Date : 3 Sep ,2021                                       */
+/************************************************************/
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+
+#define SET_BIT(REG,BIT)  (REG|=(1<<BIT))
+#define CLR_BIT(REG,BIT)  (REG&=~(1<<BIT))
+#define TOG_BIT(REG,BIT)  (REG^=(1<<BIT))
+#define GET_BIT(REG,BIT)  ((REG>>BIT)&0x01)
+
+#endif
