b775893385cfc79a7b0312459d1e045dcd45900f
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-07-09 11:25:59 +0200

    Update DCMOTOR_private.h

--- Drivers/Atmega32/HAL/DCMOTOR/DCMOTOR_private.h
+++ Drivers/Atmega32/HAL/DCMOTOR/DCMOTOR_private.h
@@ -1,3 +1,11 @@
+/************************************************************/
+/* Author : Nadine Ashraf Adeeb                             */
+/* Description: File that holds all configuration for       */
+/*                  DC MOTOR                                */
+/*                Layer : HAL                               */
+/* Version :v1.0                                            */
+/* Date : 26 Sep ,2021                                      */
+/************************************************************/
 #ifndef DCMOTOR_PRIVATE_H
 #define DCMOTOR_PRIVATE_H
 
