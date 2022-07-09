dee5a40ef73d33c45e881349c9e2fae32b1c5e3c
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-07-09 11:21:00 +0200

    Add files via upload

---
+++ Drivers/Atmega32/HAL/DCMOTOR/DCMOTOR_interface.h
@@ -0,0 +1,10 @@
+#ifndef DCMOTOR_INTERFACE_H
+#define DCMOTOR_INTERFACE_H
+
+void DCMOTOR_voidInit(void);
+
+void DCMOTOR_voidStopMotor(void);
+void DCMOTOR_voidCounterClockWise(void);
+void DCMOTOR_voidClockWise(void);
+
+#endif
