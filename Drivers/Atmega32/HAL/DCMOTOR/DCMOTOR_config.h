37d1855861bb0325f7c6cccb949468ced8bd3142
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-07-09 11:25:14 +0200

    Update DCMOTOR_config.h

--- Drivers/Atmega32/HAL/DCMOTOR/DCMOTOR_config.h
+++ Drivers/Atmega32/HAL/DCMOTOR/DCMOTOR_config.h
@@ -1,3 +1,11 @@
+/************************************************************/
+/* Author : Nadine Ashraf Adeeb                             */
+/* Description: File that holds all configuration for       */
+/*                  DC MOTOR                                */
+/*                Layer : HAL                               */
+/* Version :v1.0                                            */
+/* Date : 26 Sep ,2021                                      */
+/************************************************************/
 #ifndef DCMOTOR_CONFIG_H
 #define DCMOTOR_CONFIG_H
 
