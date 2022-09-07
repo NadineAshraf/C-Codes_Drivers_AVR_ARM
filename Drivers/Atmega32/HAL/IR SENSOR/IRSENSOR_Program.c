f2783b303a885da57953b92a6a5b6a7e4fdca346
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-07 10:46:01 +0200

    Create IRSENSOR_Program.c

---
+++ Drivers/Atmega32/HAL/IR SENSOR/IRSENSOR_Program.c
@@ -0,0 +1,32 @@
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "DIO_Interface.h"
+
+#include "IRSENSOR_Interface.h"
+#include "IRSENSOR_Register.h"
+#include "IRSENSOR_Private.h" 
+#include "IRSENSOR_Config.h"
+
+
+u8 IRSENSOR_u8GetState(u8 Copy_u8IRState)
+{
+	u8 Local_u8ErrorState = 0;
+
+	DIO_u8GetPinValue(IR_LED_u8PORT,IR_u8PIN, &Copy_u8IRState);
+
+		if(Copy_u8IRState == IRSENSOR_HIGH)
+		{
+			DIO_u8SetPinValue(IR_LED_u8PORT, LED_u8PIN, DIO_u8PIN_LOW);
+		}
+		else if(Copy_u8IRState == IRSENSOR_LOW)
+		{
+			DIO_u8SetPinValue(IR_LED_u8PORT, LED_u8PIN, DIO_u8PIN_HIGH);
+		}
+		else
+		{
+			return Local_u8ErrorState;
+		}
+
+
+}
