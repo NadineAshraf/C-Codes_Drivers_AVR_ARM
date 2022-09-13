232f03103aa08a1c7c62c4ca30ecd64af1959c81
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-13 08:26:44 +0200

    Update LED_Interface.h

--- Drivers/Atmega32/HAL/LED/LED_Interface.h
+++ Drivers/Atmega32/HAL/LED/LED_Interface.h
@@ -10,7 +10,12 @@
 #ifndef LED_INTERFACE_H_
 #define LED_INTERFACE_H_
 		
+#define SOURCE    1
+#define SINK      0
+
 void LED_u8StateON(void);
 void LED_u8StateOFF(void);
 
+u8 LED_u8ConnectionON(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8ConnectionType);
+u8 LED_u8ConnectionOFF(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8ConnectionType);
 #endif	
