d09e20bcab5aaa6a8d5609e59943363e4684e54e
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-13 08:26:07 +0200

    Update LED_Program.c

--- Drivers/Atmega32/HAL/LED/LED_Program.c
+++ Drivers/Atmega32/HAL/LED/LED_Program.c
@@ -23,7 +23,7 @@ void LED_u8StateON(void)
 	#if LED_CONNECTION_TYPE == SOURCE
 	DIO_u8SetPinValue(LED_u8PORT, LED_u8PIN, LED_HIGH);
 	#elif LED_CONNECTION_TYPE == SINK
-        DIO_u8SetPinValue(LED_u8PORT, LED_u8PIN, LED_LOW);
+    DIO_u8SetPinValue(LED_u8PORT, LED_u8PIN, LED_LOW);
 	#else
 		#error "Wrong Connection"
 	#endif
@@ -35,9 +35,43 @@ void LED_u8StateOFF(void)
 	#if LED_CONNECTION_TYPE == SOURCE
 	DIO_u8SetPinValue(LED_u8PORT, LED_u8PIN, LED_LOW);
 	#elif LED_CONNECTION_TYPE == SINK
-        DIO_u8SetPinValue(LED_u8PORT, LED_u8PIN, LED_HIGH);
+    DIO_u8SetPinValue(LED_u8PORT, LED_u8PIN, LED_HIGH);
 	#else
 		#error "Wrong Connection"
 	#endif
 
+}
+u8 LED_u8ConnectionON(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8ConnectionType)
+{
+	u8 Local_u8ErrorState = OK;
+	if(Copy_u8ConnectionType == SOURCE)
+	{
+		DIO_u8SetPinValue(Copy_u8Port, Copy_u8Pin, LED_HIGH);
+	}
+	else if(Copy_u8ConnectionType == SINK)
+	{
+		DIO_u8SetPinValue(Copy_u8Port, Copy_u8Pin, LED_LOW);
+	}
+	else
+	{
+		Local_u8ErrorState = NOK;
+	}
+	return Local_u8ErrorState;
+}
+u8 LED_u8ConnectionOFF(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8ConnectionType)
+{
+	u8 Local_u8ErrorState = OK;
+	if(Copy_u8ConnectionType == SOURCE)
+	{
+		DIO_u8SetPinValue(Copy_u8Port, Copy_u8Pin, LED_LOW);
+	}
+	else if(Copy_u8ConnectionType == SINK)
+	{
+		DIO_u8SetPinValue(Copy_u8Port, Copy_u8Pin, LED_HIGH);
+	}
+	else
+	{
+		Local_u8ErrorState = NOK;
+	}
+	return Local_u8ErrorState;
 }
