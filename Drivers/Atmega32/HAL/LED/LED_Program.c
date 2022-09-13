9adb0483e6f4bdefb7746110436d6cb652b46472
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-13 08:29:43 +0200

    Update LED_Program.c

--- Drivers/Atmega32/HAL/LED/LED_Program.c
+++ Drivers/Atmega32/HAL/LED/LED_Program.c
@@ -17,7 +17,7 @@
 #include "LED_Register.h"
 #include "LED_Private.h" 
 #include "LED_Config.h"
-
+                 /* Pre-Build Configuration*/
 void LED_u8StateON(void)
 {
 	#if LED_CONNECTION_TYPE == SOURCE
@@ -41,6 +41,7 @@ void LED_u8StateOFF(void)
 	#endif
 
 }
+              /* Post-Buid Configuration*/
 u8 LED_u8ConnectionON(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8ConnectionType)
 {
 	u8 Local_u8ErrorState = OK;
