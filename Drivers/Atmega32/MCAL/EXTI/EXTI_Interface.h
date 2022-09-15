b616dc0ac000c1458176232cf1391c6320d7cf4e
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-15 14:45:21 +0200

    Update EXTI_Interface.h

--- Drivers/Atmega32/MCAL/EXTI/EXTI_Interface.h
+++ Drivers/Atmega32/MCAL/EXTI/EXTI_Interface.h
@@ -15,7 +15,9 @@
 #define  FALLING_EDGE  3
 #define  RISING_EDGE   4
 
-
+#define INT0 6
+#define INT1 7
+#define INT2 5
 void EXTI_voidINT0Init(void);
 void EXTI_voidINT1Init(void);
 void EXTI_voidINT2Init(void);
@@ -24,6 +26,8 @@ u8 EXTI_u8INT0SetSenseControl(u8 Copy_u8Sense);
 u8 EXTI_u8INT1SetSenseControl(u8 Copy_u8Sense);
 u8 EXTI_u8INT2SetSenseControl(u8 Copy_u8Sense);
 
+u8 EXTI_u8EnableInterrupt(u8 Copy_InterruptNumber);
+u8 EXTI_u8DisableInterrupt(u8 Copy_InterruptNumber);
 #define ENABLE  1
 #define DISABLE 2
 
