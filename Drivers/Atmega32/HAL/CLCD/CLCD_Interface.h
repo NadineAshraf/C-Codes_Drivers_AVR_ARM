55bda5f3eaa50694dbc332200ca56c23bcebe129
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-16 08:37:25 +0200

    Update CLCD_Interface.h

--- Drivers/Atmega32/HAL/CLCD/CLCD_Interface.h
+++ Drivers/Atmega32/HAL/CLCD/CLCD_Interface.h
@@ -17,4 +17,5 @@ void CLCD_voidSendData(u8 Copy_u8Data);
 void CLCD_voidSendString(const char* Copy_pcString);
 void CLCD_voidGoToXY(u8 Copy_u8XPos, u8 Copy_u8YPos);
 void CLCD_voidWriteSpecialCharacter(u8* Copy_pu8Pattern, u8 Copy_u8PatternNumber, u8 Copy_u8XPos, u8 Copy_u8YPos);
+void CLCD_voidWriteNumber(s32 Copy_s32Number);
 #endif
