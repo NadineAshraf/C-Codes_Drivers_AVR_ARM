3b2ce2ff4d95fffac83decbee3d6f5d86cd46fcb
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-16 11:20:03 +0200

    Update EXTI_Interface.h

--- Drivers/Atmega32/MCAL/EXTI/EXTI_Interface.h
+++ Drivers/Atmega32/MCAL/EXTI/EXTI_Interface.h
@@ -15,9 +15,7 @@
 #define  FALLING_EDGE  3
 #define  RISING_EDGE   4
 
-#define INT0 6
-#define INT1 7
-#define INT2 5
+
 void EXTI_voidINT0Init(void);
 void EXTI_voidINT1Init(void);
 void EXTI_voidINT2Init(void);
@@ -26,8 +24,6 @@ u8 EXTI_u8INT0SetSenseControl(u8 Copy_u8Sense);
 u8 EXTI_u8INT1SetSenseControl(u8 Copy_u8Sense);
 u8 EXTI_u8INT2SetSenseControl(u8 Copy_u8Sense);
 
-u8 EXTI_u8EnableInterrupt(u8 Copy_InterruptNumber);
-u8 EXTI_u8DisableInterrupt(u8 Copy_InterruptNumber);
 #define ENABLE  1
 #define DISABLE 2
 
@@ -35,8 +31,12 @@ void EXTI_voidINT0State(u8 Copy_u8Int0State);
 void EXTI_voidINT1State(u8 Copy_u8Int0State);
 void EXTI_voidINT2State(u8 Copy_u8Int0State);
 
-void EXTI_voidINT0SetCallBack(void (*Copy_pv0NotificationFunction)(void));
-void EXTI_voidINT1SetCallBack(void (*Copy_pv1NotificationFunction)(void));
-void EXTI_voidINT2SetCallBack(void (*Copy_pv2NotificationFunction)(void));
+#define INT0    0
+#define INT1    1
+#define INT2    2
+void EXTI_voidINTSetCallBack(void (*Copy_pvNotificationFunction)(void), u8 Copy_u8INTNumber);
+
+
+#endif
 
 #endif
