fac3c003b83ded424876cb89f58c75292e394e74
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-16 14:29:47 +0200

    Update main.c

--- Drivers/Atmega32/APP/EXTI_TestDriver/main.c
+++ Drivers/Atmega32/APP/EXTI_TestDriver/main.c
@@ -8,7 +8,6 @@
 #include "BIT_MATH.h"
 
 #include "DIO_Interface.h"
-
 #include "EXTI_Interface.h"
 #include "GIE_Interface.h"
 void INT0_ISR(void);
@@ -17,10 +16,10 @@ void main (void)
 {
 
 	DIO_u8SetPinDirection(DIO_u8PORTD,DIO_u8PIN0, DIO_u8PIN_OUTPUT);
-	DIO_u8SetPinValue(DIO_u8PORTD,DIO_u8PIN0, PULLUP);
+	DIO_u8SetPinDirection(DIO_u8PORTD,DIO_u8PIN2, DIO_u8PIN_INPUT);
+	DIO_u8SetPinValue(DIO_u8PORTD,DIO_u8PIN2, PULLUP);
 
-	EXTI_voidINT0SetCallBack(&INT0_ISR);
-	//EXTI_voidINT0Init();
+	EXTI_voidINTSetCallBack(&INT0_ISR, INT0);
 	EXTI_voidINT0State(ENABLE);
 	EXTI_u8INT0SetSenseControl(FALLING_EDGE);
 	GIE_voidEnable();
@@ -31,6 +30,7 @@ void main (void)
 }
 void INT0_ISR(void)
 {
-	//DIO_u8SetPinValue(DIO_u8PORTD, DIO_u8PIN2 , DIO_u8PIN_HIGH);
+
 	DIO_u8SetPinValue(DIO_u8PORTD, DIO_u8PIN0 , DIO_u8PIN_HIGH);
+
 }
