3cc4a28d569c3d35d89ec79c061bebd956d63f5a
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-09-17 10:41:49 +0200

    Update DIO_Program.c

--- Drivers/Atmega32/MCAL/DIO/DIO_Program.c
+++ Drivers/Atmega32/MCAL/DIO/DIO_Program.c
@@ -18,7 +18,7 @@
 
 u8 DIO_u8SetPinDirection(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Direction)
 {   
-    u8 Local_u8ErrorState = 0;
+    u8 Local_u8ErrorState = OK;
 	if(Copy_u8Pin <= DIO_u8PIN7 )
 	{
 		if(Copy_u8Direction == DIO_u8PIN_INPUT)
@@ -29,7 +29,7 @@ u8 DIO_u8SetPinDirection(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Direction)
 				case DIO_u8PORTB: CLR_BIT(DDRB, Copy_u8Pin);
 				case DIO_u8PORTC: CLR_BIT(DDRC, Copy_u8Pin);
 				case DIO_u8PORTD: CLR_BIT(DDRD, Copy_u8Pin);
-				default: Local_u8ErrorState = 1;break;
+				default: Local_u8ErrorState = NOK;break;
 			}
 		}
 		else if(Copy_u8Direction == DIO_u8PIN_OUTPUT)
@@ -40,38 +40,38 @@ u8 DIO_u8SetPinDirection(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Direction)
 				case DIO_u8PORTB: SET_BIT(DDRB, Copy_u8Pin);
 				case DIO_u8PORTC: SET_BIT(DDRC, Copy_u8Pin);
 				case DIO_u8PORTD: SET_BIT(DDRD, Copy_u8Pin);
-				default: Local_u8ErrorState = 1;break;
+				default: Local_u8ErrorState = NOK;break;
 			}
 		}
 		else
 	    {
-		   Local_u8ErrorState = 1;
+		   Local_u8ErrorState = NOK;
 	    }
 	}
 	else
 	{
-		Local_u8ErrorState = 1;
+		Local_u8ErrorState = NOK;
 	}
 	
 	return Local_u8ErrorState;
 }
 u8 DIO_u8SetPortDirection(u8 Copy_u8Port, u8 Copy_u8Direction)
 {
-	u8 Local_u8ErrorState = 0;
+	u8 Local_u8ErrorState = OK;
 			switch(Copy_u8Port)
 			{
 				case DIO_u8PORTA: DDRA = Copy_u8Direction;break;
 				case DIO_u8PORTB: DDRB = Copy_u8Direction;break;
 				case DIO_u8PORTC: DDRC = Copy_u8Direction;break;
 				case DIO_u8PORTD: DDRD = Copy_u8Direction;break;
-				default: Local_u8ErrorState = 1;break;
+				default: Local_u8ErrorState = NOK;break;
 			}
 		
 	 return Local_u8ErrorState;
 }
 u8 DIO_u8SetPinValue(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Value)
 {
-	 u8 Local_u8ErrorState = 0;
+	 u8 Local_u8ErrorState = OK;
 	 if(Copy_u8Pin <= DIO_u8PIN7 )
 	{
 		if(Copy_u8Value == DIO_u8PIN_LOW)
@@ -82,7 +82,7 @@ u8 DIO_u8SetPinValue(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Value)
 				case DIO_u8PORTB: CLR_BIT(PORTB, Copy_u8Pin);
 				case DIO_u8PORTC: CLR_BIT(PORTC, Copy_u8Pin);
 				case DIO_u8PORTD: CLR_BIT(PORTD, Copy_u8Pin);
-				default: Local_u8ErrorState = 1;break;
+				default: Local_u8ErrorState = NOK;break;
 			}
 		}
 		else if(Copy_u8Value == DIO_u8PIN_HIGH)
@@ -98,32 +98,32 @@ u8 DIO_u8SetPinValue(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Value)
 		}
 		else
 	    {
-		   Local_u8ErrorState = 1;
+		   Local_u8ErrorState = NOK;
 	    }
 	}
 	else
 	{
-		Local_u8ErrorState = 1;
+		Local_u8ErrorState = NOK;
 	}
 	
 	 return Local_u8ErrorState;
 }
 u8 DIO_u8SetPortValue(u8 Copy_u8Port, u8 Copy_u8Value)
 {
-	 u8 Local_u8ErrorState = 0;
+	 u8 Local_u8ErrorState = OK;
 	 switch(Copy_u8Port)
 			{
 				case DIO_u8PORTA: PORTA = Copy_u8Value;break;
 				case DIO_u8PORTB: PORTB = Copy_u8Value;break;
 				case DIO_u8PORTC: PORTC = Copy_u8Value;break;
 				case DIO_u8PORTD: PORTD = Copy_u8Value;break;
-				default: Local_u8ErrorState = 1;break;
+				default: Local_u8ErrorState = NOK;break;
 			}
 	 return Local_u8ErrorState;
 }
 u8 DIO_u8GetPinValue(u8 Copy_u8Port, u8 Copy_u8Pin, u8* Copy_pu8Value)
 {
-	 u8 Local_u8ErrorState = 0;
+	 u8 Local_u8ErrorState = OK;
 	 if((Copy_pu8Value != NULL) && (Copy_u8Pin <= DIO_u8PIN7))
 	 {
 		 switch(Copy_u8Port)
@@ -132,12 +132,12 @@ u8 DIO_u8GetPinValue(u8 Copy_u8Port, u8 Copy_u8Pin, u8* Copy_pu8Value)
 				case DIO_u8PORTB: *Copy_pu8Value = GET_BIT(PINB, Copy_u8Pin);break;
 				case DIO_u8PORTC: *Copy_pu8Value = GET_BIT(PINC, Copy_u8Pin);break;
 				case DIO_u8PORTD: *Copy_pu8Value = GET_BIT(PIND, Copy_u8Pin);break;
-				default: Local_u8ErrorState = 1;break;
+				default: Local_u8ErrorState = NOK;break;
 			}
 	 }
 	 else
 	 {
-		 Local_u8ErrorState = 1;
+		 Local_u8ErrorState = NOK;
 	 }
 	 return Local_u8ErrorState;
 }
